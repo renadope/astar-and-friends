9ba65d9779be33115c295b84c1baf107ae57cb27
renadope <renadoparia@gmail.com>
2025-05-14 00:28:14 -0400

    need some sleep

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,7 @@
 import type {Route} from "./+types/home";
+import {aStar} from "~/services/aStar";
+import {manhattan} from "~/utils/heuristics";
+import {useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -8,9 +11,31 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const grid = [
+        [12, 9001, 50],
+        [9002, 28, 11000],
+        [45, 800, 11212],
+    ]
+
+    const aStarResult = aStar(grid, [0, 0], [grid.length - 1, grid[0].length - 1], manhattan, {
+        allowed: true,
+        cornerCutting: 'lax'
+    })
+
+    if (!aStarResult.success) {
+        return (
+            <div className={'bg-red-500'}>
+                <p className={'text-7xl'}>Sad</p>
+            </div>
+        )
+    }
+
     return (
-        <div className={'bg-sky-500'}>
-            <p>Foo</p>
+        <div>
+            {JSON.stringify(aStarResult.value,null,2)}
         </div>
     )
 }
+
+
+
