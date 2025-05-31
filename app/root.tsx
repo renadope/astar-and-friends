80e28b57f6f0c490bef00f254c6c1b7624d9fad1
renadope <renadoparia@gmail.com>
2025-05-31 15:15:40 -0400

    change favicon

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,7 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
