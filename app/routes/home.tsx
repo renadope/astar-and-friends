54d950a85b35b21eeb941de77450c52f1b8f7e49
renadope <renadoparia@gmail.com>
2025-06-02 10:50:17 -0400

    added footer content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,8 @@ import type {Route} from "./+types/home";
 import {GridProvider} from "~/state/context";
 import Grid from "~/components/grid";
 import ControlPanel from "~/components/control-panel/control-panel";
+import {Link} from "react-router";
+import {GithubIcon, LinkedInIcon} from "~/components/icons/icons";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -25,6 +27,56 @@ export function Main() {
                     </div>
                 </div>
             </div>
+            {/*<footer className="text-center text-sm text-gray-500 mt-12 pb-4 border-t pt-4 border-gray-300">*/}
+            {/*    <div className="flex flex-col items-center justify-center space-y-2">*/}
+            {/*        <div className="flex items-center space-x-1">*/}
+            {/*            <p className="flex items-center space-x-1">*/}
+            {/*                <span>Built with ⚡️ by</span>*/}
+            {/*                <Link*/}
+            {/*                    to="https://github.com/renadope"*/}
+            {/*                    target="_blank"*/}
+            {/*                    rel="noopener noreferrer"*/}
+            {/*                    className="underline hover:text-gray-700 transition-colors"*/}
+            {/*                >*/}
+            {/*                    <span className={`bg-gradient-to-r*/}
+            {/*                     from-purple-400 via-fuchsia-400 to-pink-400 bg-clip-text text-transparent*/}
+            {/*                     hover:from-pink-400 hover:via-fuchsia-400 hover:to-purple-400 transition-all duration-500`}>Renado</span>*/}
+            {/*                </Link>*/}
+            {/*            </p>*/}
+
+            {/*            <span>·</span>*/}
+
+            {/*            <Link*/}
+            {/*                to="https://www.linkedin.com/in/renadope"*/}
+            {/*                target="_blank"*/}
+            {/*                rel="noopener noreferrer"*/}
+            {/*                aria-label="LinkedIn Profile"*/}
+            {/*            >*/}
+            {/*                <LinkedInIcon className={'size-5 hover:size-7 transition-all duration-300'}/>*/}
+            {/*            </Link>*/}
+
+            {/*            <span>·</span>*/}
+
+            {/*            <Link*/}
+            {/*                to="https://www.linkedin.com/in/renadope"*/}
+            {/*                target="_blank"*/}
+            {/*                rel="noopener noreferrer"*/}
+            {/*                aria-label="LinkedIn Profile"*/}
+            {/*            >*/}
+
+            {/*            </Link>*/}
+            {/*            <GithubIcon className={'size-5 hover:size-7 transition-all duration-300'}/>*/}
+            {/*            <span>·</span>*/}
+
+
+            {/*        </div>*/}
+
+            {/*        <p className="text-xs">*/}
+            {/*            © {new Date().getFullYear()}*/}
+            {/*            /!*Renado. All rights reserved.*!/*/}
+            {/*        </p>*/}
+            {/*    </div>*/}
+            {/*</footer>*/}
         </div>
     );
 }
